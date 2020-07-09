985cba9a3a42d9b6884083eaef69b8ce98c8a902
erezpoliak <poliakerez@gmail.com>
2020-07-09 20:52:36 +0300

    include component to handle comment section

--- src/components/CommentSection.js
+++ src/components/CommentSection.js
@@ -1,8 +1,116 @@
-import React from "react";
+import React, { useRef, useState, useContext } from "react";
 import styled from "styled-components";
+import { Insta_Context } from "./Context";
+import Button from "@material-ui/core/Button";
+import * as Api from "./Api";
 
-const CommentSection = () => {
-  return <div></div>;
+const CommentSection = ({ photo }) => {
+  const inputRef = useRef();
+  const { comments, set_comments, currentUser } = useContext(Insta_Context);
+  const [newComment, set_newComment] = useState("");
+
+  const getCommentsForPhoto = (photo) => {
+    const commentsForPhoto = comments.filter(
+      (comment) => comment.photo._id === photo._id
+    );
+    return commentsForPhoto.map((comment) => {
+      if (
+        comment.user._id === currentUser._id ||
+        comment.commentedUser._id === currentUser._id
+      ) {
+        console.log(comment.commentedUser);
+        return (
+          <CommentWrapper>
+            <FlexWrapper>
+              <ProfilePhoto
+                src={
+                  comment && comment.commentedUser
+                    ? comment.commentedUser.profilePhoto
+                    : ""
+                }
+              />
+            </FlexWrapper>
+            <div style={{ fontWeight: "900" }}>
+              {comment.commentedUser.userName}
+            </div>
+            <div>{comment.comment}</div>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => deleteComment(comment._id)}
+            >
+              x
+            </Button>
+          </CommentWrapper>
+        );
+      } else {
+        return (
+          <CommentWrapper>
+            <FlexWrapper>
+              <ProfilePhoto src={comment.commentedUser.profilePhoto} />
+            </FlexWrapper>
+            <div style={{ fontWeight: "900" }}>
+              {comment.commentedUser.userName}
+            </div>
+            <div>{comment.comment}</div>
+          </CommentWrapper>
+        );
+      }
+    });
+  };
+
+  const uploadNewComment = (photo) => async (e) => {
+    e.preventDefault();
+    await Api.uploadNewComment(photo, newComment, currentUser);
+    inputRef.current.value = "";
+    const newComments = await Api.getComments();
+    set_comments(newComments);
+  };
+
+  const deleteComment = async (commentId) => {
+    await Api.deleteComment(commentId);
+    const newComments = await Api.getComments();
+    set_comments(newComments);
+  };
+
+  return (
+    <>
+      {getCommentsForPhoto(photo)}
+      <br />
+      <br />
+      <form>
+        <label>Add a comment</label>
+        <input
+          type="text"
+          onChange={(e) => set_newComment(e.target.value)}
+          ref={inputRef}
+        />
+        <input type="submit" onClick={uploadNewComment(photo)} />
+      </form>
+    </>
+  );
 };
 
 export default CommentSection;
+
+const FlexWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: red;
+`;
+
+const CommentWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 20%);
+  justify-content: space-around;
+  align-items: center;
+  padding-top: 2.5vh;
+`;
+
+const ProfilePhoto = styled.img`
+  /* height: 100%; */
+  width: 60%;
+  border: 1px solid black;
+  border-radius: 50%;
+`;
--- src/components/Feed.js
+++ src/components/Feed.js
@@ -6,8 +6,7 @@ import * as Api from "./Api";
 import { Insta_Context } from "./Context";
 import Button from "@material-ui/core/Button";
 import { ThumbsUp } from "@styled-icons/fa-solid/ThumbsUp";
-import { TruckMonster } from "styled-icons/fa-solid";
-// import Async from 'react-async';
+import CommentSection from "./CommentSection";
 
 const Feed = () => {
   const {
@@ -19,101 +18,11 @@ const Feed = () => {
     likes,
     set_likes,
   } = useContext(Insta_Context);
-  const [commentsForRender, set_commentsForRender] = useState();
-  const [newComment, set_newComment] = useState("");
-  const inputRef = useRef();
-
-  // useEffect(() => {
-  //   const buildRenderedComments = async () => {
-  //     const commentsForPhoto = [...comments];
-  //     const commentsArr = await Promise.all(
-  //       commentsForPhoto.map(async (comment) => {
-  //         const CommentedUser = await Api.getUserById(comment.commentedUserId);
-  //         const user = await Api.getUserById(comment.userId);
-  //         return {
-  //           CommentedUser,
-  //           comment: comment.comment,
-  //           photoId: comment.photoId,
-  //           id: comment._id,
-  //           user,
-  //         };
-  //       })
-  //     );
-  //     set_commentsForRender(commentsArr);
-  //   };
-  //   buildRenderedComments();
-  // }, [comments]);
-
-  // useEffect(() => {}, [comments]);
-
-  const deleteComment = async (commentId) => {
-    await Api.deleteComment(commentId);
-    const newComments = await Api.getComments();
-    set_comments(newComments);
-  };
 
   const deletePhoto = async (photoId) => {
     await Api.deletePhotoByPhotoId(photoId);
     const newPhotos = await Api.getPhotos();
     set_photos(newPhotos);
-  };
-
-  const uploadNewComment = (photo) => async (e) => {
-    e.preventDefault();
-    await Api.uploadNewComment(photo, newComment, currentUser);
-    inputRef.current.value = "";
-    const newComments = await Api.getComments();
-    set_comments(newComments);
-  };
-
-  const getCommentsForPhoto = (photo) => {
-    const commentsForPhoto = comments.filter(
-      (comment) => comment.photo._id === photo._id
-    );
-    return commentsForPhoto.map((comment) => {
-      if (
-        comment.user._id === currentUser._id ||
-        comment.commentedUser._id === currentUser._id
-      ) {
-        console.log(comment.commentedUser);
-        return (
-          <CommentWrapper>
-            <FlexWrapper>
-              <ProfilePhoto
-                src={
-                  comment && comment.commentedUser
-                    ? comment.commentedUser.profilePhoto
-                    : ""
-                }
-              />
-            </FlexWrapper>
-            <div style={{ fontWeight: "900" }}>
-              {comment.commentedUser.userName}
-            </div>
-            <div>{comment.comment}</div>
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={() => deleteComment(comment._id)}
-            >
-              x
-            </Button>
-          </CommentWrapper>
-        );
-      } else {
-        return (
-          <CommentWrapper>
-            <FlexWrapper>
-              <ProfilePhoto src={comment.commentedUser.profilePhoto} />
-            </FlexWrapper>
-            <div style={{ fontWeight: "900" }}>
-              {comment.commentedUser.userName}
-            </div>
-            <div>{comment.comment}</div>
-          </CommentWrapper>
-        );
-      }
-    });
   };
 
   const checkIfLiked = (photo) => {
@@ -204,28 +113,13 @@ const Feed = () => {
                     <img src={photo.url} width="60%" alt={photo.title} />
                   </FlexWrapper>
                   {likes.length > 0 ? getLikedBy(photo) : ""}
-                  {/* put last like of the array and then show the other in number of likes, make sure u can like dislike */}
-                  {comments ? getCommentsForPhoto(photo) : ""}
-                  <br />
-                  <br />
-                  <form>
-                    <label>Add a comment</label>
-                    <input
-                      type="text"
-                      onChange={(e) => set_newComment(e.target.value)}
-                      ref={inputRef}
-                    />
-                    <input type="submit" onClick={uploadNewComment(photo)} />
-                  </form>
-                  {checkIfLiked(photo) ? (
-                    <LikeBtn onClick={() => changeLike(photo)} liked={true}>
-                      <LikeIcon />
-                    </LikeBtn>
-                  ) : (
-                    <LikeBtn onClick={() => changeLike(photo)} liked={false}>
-                      <LikeIcon />
-                    </LikeBtn>
-                  )}
+                  {comments ? <CommentSection photo={photo} /> : ""}
+                  <LikeBtn
+                    onClick={() => changeLike(photo)}
+                    liked={checkIfLiked(photo)}
+                  >
+                    <LikeIcon />
+                  </LikeBtn>
                 </CardContent>
               </Card>
             );
